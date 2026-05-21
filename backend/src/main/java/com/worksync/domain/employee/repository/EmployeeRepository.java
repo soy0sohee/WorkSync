f7cfd190f37b84893fd71f32474472abd2a62ab5
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-05-21 16:13:31 +0900

    사용자 조회 코드 추가

--- backend/src/main/java/com/worksync/domain/employee/repository/EmployeeRepository.java
+++ backend/src/main/java/com/worksync/domain/employee/repository/EmployeeRepository.java
@@ -1,4 +1,10 @@
 package com.worksync.domain.employee.repository;
 
-public interface EmployeeRepository {
+import com.worksync.domain.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
 }
