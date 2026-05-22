0031373822e00b8a3b185f80d4e5ceedb957e1c2
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-05-22 15:11:31 +0900

    JpaRepository 상속

--- backend/src/main/java/com/worksync/domain/department/repository/DepartmentRepository.java
+++ backend/src/main/java/com/worksync/domain/department/repository/DepartmentRepository.java
@@ -1,4 +1,7 @@
 package com.worksync.domain.department.repository;
 
-public interface DepartmentRepository {
+import com.worksync.domain.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
