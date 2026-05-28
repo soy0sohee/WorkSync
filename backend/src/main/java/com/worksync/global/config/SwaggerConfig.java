640a3e416a5511f8272e3297aead6a84e1e4e85f
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-05-28 11:41:29 +0900

    SwaggerConfig파일 추가

---
+++ backend/src/main/java/com/worksync/global/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.worksync.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtScheme = "BearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("WorkSync API")
+                        .description("WorkSync 그룹웨어 API 명세서")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(jwtScheme))
+                .components(new Components()
+                        .addSecuritySchemes(jwtScheme, new SecurityScheme()
+                                .name(jwtScheme)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
